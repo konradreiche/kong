7c363adfcefd1e9c0fe14737873b8ae152d7c63e
Konrad Reiche <konrad.reiche@reddit.com>
2022-10-17 13:28:34 -0700

    Add test for NewSprint

--- model_test.go
+++ model_test.go
@@ -3,6 +3,7 @@ package kong
 import (
 	"errors"
 	"testing"
+	"time"
 
 	"github.com/andygrunwald/go-jira"
 	"github.com/google/go-cmp/cmp"
@@ -147,6 +148,46 @@ func TestNewIssues(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestNewSprint(t *testing.T) {
+	t.Run("without-end-date", func(t *testing.T) {
+		sprint := jira.Sprint{
+			ID:    1,
+			Name:  "Komodo",
+			State: "active",
+		}
+		got := NewSprint(sprint)
+		want := Sprint{
+			ID:    1,
+			Name:  "Komodo",
+			State: "active",
+		}
+		if diff := cmp.Diff(got, want); diff != "" {
+			t.Errorf("diff: %s", diff)
+		}
+	})
+
+	t.Run("with-end-date", func(t *testing.T) {
+		sprint := jira.Sprint{
+			ID:    1,
+			Name:  "Komodo",
+			State: "active",
+		}
+		endDate := time.Date(1933, time.April, 7, 0, 0, 0, 0, time.UTC)
+		sprint.EndDate = &endDate
+
+		got := NewSprint(sprint)
+		want := Sprint{
+			ID:      1,
+			Name:    "Komodo",
+			State:   "active",
+			EndDate: endDate,
+		}
+		if diff := cmp.Diff(got, want); diff != "" {
+			t.Errorf("diff: %s", diff)
+		}
+	})
 }
 
 func transition(id, name, description, acronym string) Transition {
